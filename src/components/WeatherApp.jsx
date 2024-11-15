30ef9b08c5ef7b9bc897e9ac1d1cd8e2c68bcafa
Ian Pollack <ianjpollack@gmail.com>
2024-11-14 18:35:31 -0800

    weather api fetch setup

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,4 +1,4 @@
-// import React from 'react'
+import { useState } from "react";
 import sunny from "../assets/images/sunny.png";
 import cloudy from "../assets/images/cloudy.png";
 import rainy from "../assets/images/rainy.png";
@@ -6,23 +6,48 @@ import snowy from "../assets/images/snowy.png";
 // import loading from "../assets/images/loading.gif";
 
 const WeatherApp = () => {
+  const [data, setData] = useState([]);
+  const [location, setLocation] = useState("");
+  const [units, setUnits] = useState("imperial");
+
+  const api_key = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+  
+  const search = async () => {
+    const url = `${import.meta.env.VITE_OPEN_WEATHER_CITY_URL}?q=${location}&units=${units}&appid=${api_key}`;
+    const response = await fetch(url);
+    const searchData = await response.json();
+    console.log(searchData);
+    setData(searchData);
+    setLocation("");
+  }
+
+  const handleEnterKey = (e) => {
+    if (e.key === "Enter") {
+      search();
+    };
+  };
+
   return (
     <div className="container">
       <div className="weather-app">
         <div className="search">
           <div className="search-top">
             <i className="fa-solid fa-location-dot" />
-            <div className="location">London</div>
+            <div className="location">{data.name}</div>
           </div>
           <div className="search-bar">
-            <input type="text" placeholder="Enter Location" />
-            <i className="fa-solid fa-magnifying-glass" />
+            <input type="text" placeholder="Enter Location" value={location} onChange={handleInputChange} onKeyDown={handleEnterKey} />
+            <i className="fa-solid fa-magnifying-glass" onClick={search} />
           </div>
         </div>
         <div className="weather">
           <img src={sunny} alt="sunny" />
-          <div className="weather-type">Clear</div>
-          <div className="temp">28°</div>
+          <div className="weather-type">{data.weather ? data.weather[0].main : null}</div>
+          <div className="temp">{data.main ? `${Math.floor(data.main.temp)}°${units === "imperial" && "F"}${units === "metric" && "C"}` : null}</div>
         </div>
         <div className="weather-date">
           <p>Wed, Nov 13th, 2024</p>
@@ -31,12 +56,12 @@ const WeatherApp = () => {
           <div className="humidity">
             <div className="data-name">Humidity</div>
             <i className="fa-solid fa-droplet" />
-            <div className="data">55%</div>
+            <div className="data">{data.main ? data.main.humidity : null}%</div>
           </div>
           <div className="wind">
             <div className="data-name">Wind</div>
             <i className="fa-solid fa-wind" />
-            <div className="data">3 km/h</div>
+            <div className="data">{data.wind ? data.wind.speed : null} {units === "imperial" && "mph"}{units === "metric" && "km/h"}</div>
           </div>
 
         </div>
